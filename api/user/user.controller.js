1031454e9339d9e0a00a01a79b87b0b63cb7ea1c
Julian Fauricio Valencia Marin <jvalencia@reefstudios.co>
2019-08-17 16:01:05 -0500

    add service update password

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -5,67 +5,69 @@ const services = require('./user.services');
 
 exports.saveUser = (req, res) => {
   const data = req.body;
-  return services.saveUserPromise(data)
+  return services
+    .saveUserPromise(data)
     .then(response => res.json(response))
     .catch(err => res.json(err));
 };
 
 exports.updateUser = (req, res) => {
-  const {
-    id
-  } = req.params;
-  const {
-    body
-  } = _.pick(req.body, ['email', 'password']);
-  return services.updateUserPromise(id, body)
-    .then(response => res.json({
-      ...response,
-      ok: true
-    }))
+  const { id } = req.params;
+  // const { body } = _.pick(req.body, ['email', 'password']);
+  return services
+    .updateUserPromise(id, req.body)
+    .then(response =>
+      res.json({
+        ...response,
+        ok: true
+      })
+    )
     .catch(err => res.status(400).json(err));
 };
 
 exports.getUsers = (req, res) => {
-  return services.getUsersPromise()
+  return services
+    .getUsersPromise()
     .then(response => {
-      return res.status(200).json(
-        response
-      )
+      return res.status(200).json(response);
     })
     .catch(err => res.status(400).json(err));
 };
 exports.getUSerByCedulaUpdateSearchHistory = (req, res) => {
   let cedula = req.params.cedula;
   let dataUser = req.user;
-  return services.getUSerByCedulaUpdateSearchHistoryPromise(cedula, dataUser)
-    .then((response) => {
+  return services
+    .getUSerByCedulaUpdateSearchHistoryPromise(cedula, dataUser)
+    .then(response => {
       return res.json({
         ...response,
         ok: true
-      })
+      });
     })
-    .catch((e) => res.status(400).json(e))
-}
+    .catch(e => res.status(400).json(e));
+};
 exports.getUSerByCedula = (req, res) => {
   let cedula = req.params.cedula;
-  return services.getUSerByCedulaPromise(cedula)
-    .then((response) => {
+  return services
+    .getUSerByCedulaPromise(cedula)
+    .then(response => {
       return res.json({
         ...response,
         ok: true
-      })
+      });
     })
-    .catch((e) => res.status(400).json(e))
-}
+    .catch(e => res.status(400).json(e));
+};
 
 exports.deleteUser = (req, res) => {
-  const {
-    id
-  } = req.params;
-  return services.deleteUSerPromise(id)
-    .then(response => res.json({
-      ...response,
-      ok: true
-    }))
+  const { id } = req.params;
+  return services
+    .deleteUSerPromise(id)
+    .then(response =>
+      res.json({
+        ...response,
+        ok: true
+      })
+    )
     .catch(err => res.status(400).json(err));
 };
--- api/user/user.services.js
+++ api/user/user.services.js
@@ -4,108 +4,125 @@ const bcrypt = require('bcrypt');
 // const _ = require('underscore');
 
 const User = require('./user.model');
-exports.saveUserPromise = data => new Promise((resolve, reject) => {
-  const temp = {
-    ...data,
-    password: bcrypt.hashSync(data.password, 10),
-  };
-  const user = new User(temp);
+exports.saveUserPromise = data =>
+  new Promise((resolve, reject) => {
+    const temp = {
+      ...data,
+      password: bcrypt.hashSync(data.password, 10)
+    };
+    const user = new User(temp);
 
-  User.findOne({
-    email: data.email
-  }, (err, userDB) => {
-    if (err) {
-      const err = {
-        ok: false,
-        err
+    User.findOne(
+      {
+        email: data.email
+      },
+      (err, userDB) => {
+        if (err) {
+          const err = {
+            ok: false,
+            err
+          };
+          reject(err);
+        }
+        if (userDB) {
+          let body = {
+            ok: false,
+            message: 'el correo existe nene'
+          };
+          return resolve(body);
+        }
+        if (!userDB) {
+          user.save((err, userDB) => {
+            if (err) {
+              const error = {
+                ok: false,
+                err,
+                status: 400
+              };
+              return reject(error);
+            }
+            const ok = {
+              userDB,
+              ok: true
+            };
+            return resolve(ok);
+          });
+        }
       }
-      reject(err);
+    );
+  });
+
+exports.updateUserPromise = (id, body) =>
+  new Promise((resolve, reject) => {
+    console.log('body: ', body);
+    let newBody = { ...body };
+    if (body.password) {
+      newBody = {
+        password: bcrypt.hashSync(body.password, 10)
+      };
     }
-    if (userDB) {
-      let body = {
-        ok: false,
-        message: 'el correo existe nene'
-      }
-      return resolve(body)
-    }
-    if (!userDB) {
-      user.save((err, userDB) => {
+    console.log('newBody: ', newBody);
+    User.findByIdAndUpdate(
+      id,
+      newBody,
+      {
+        new: true,
+        runValidators: true
+      },
+      (err, userDB) => {
         if (err) {
           const error = {
             ok: false,
             err,
-            status: 400,
+            status: 400
           };
           return reject(error);
         }
-        const ok = {
-          userDB,
-          ok: true,
-        };
-        return resolve(ok);
-      });
-
-    }
-  })
-});
-
-exports.updateUserPromise = (id, body) => new Promise((resolve, reject) => {
-  User.findByIdAndUpdate(id, body, {
-    new: true,
-    runValidators: true
-  }, (err, userDB) => {
-    if (err) {
-      const error = {
-        ok: false,
-        err,
-        status: 400,
-      };
-      return reject(error);
-    }
-    return resolve(userDB);
+        return resolve(userDB);
+      }
+    );
   });
-});
 
 // exports.getUsersPromise = (sky, lim) => new Promise((resolve, reject) => {
-exports.getUsersPromise = () => new Promise((resolve, reject) => {
-  User.find({
+exports.getUsersPromise = () =>
+  new Promise((resolve, reject) => {
+    User.find({
       state: true
     })
-    .populate('searchHistory')
-    .exec((err, users) => {
-      if (err) {
-        const error = {
-          ok: false,
-          err,
-          status: 400,
-        };
-        return reject(error);
-      }
-      return resolve({
-        status: 200,
-        ok: true,
-        data: users
+      .populate('searchHistory')
+      .exec((err, users) => {
+        if (err) {
+          const error = {
+            ok: false,
+            err,
+            status: 400
+          };
+          return reject(error);
+        }
+        return resolve({
+          status: 200,
+          ok: true,
+          data: users
+        });
       });
-    });
-});
-exports.getUSerByCedulaUpdateSearchHistoryPromise = (cedula, dataUSer) => new Promise((resolve, reject) => {
-  User.find({
+  });
+exports.getUSerByCedulaUpdateSearchHistoryPromise = (cedula, dataUSer) =>
+  new Promise((resolve, reject) => {
+    User.find({
       cedula
-    })
-    .exec((err, usersDB) => {
+    }).exec((err, usersDB) => {
       if (err) {
         let err = {
           ok: false,
-          err,
-        }
+          err
+        };
         return reject(err);
       }
       if (usersDB.length === 0) {
-
         const error = {
           ok: false,
           message: 'no existe el usuario mi rey',
-          status: 400,
+          status: 400
         };
         return reject(error);
       }
@@ -114,39 +131,43 @@ exports.getUSerByCedulaUpdateSearchHistoryPromise = (cedula, dataUSer) => new Pr
           cedula,
           name: dataUSer.name,
           lastName: dataUSer.lastName
-        })
+        });
         let newArray = {
-          ...dataUSer,
-        }
-        User.findOneAndUpdate({
-          email: dataUSer.email
-        }, newArray, null, (err, doc, any) => {
-          return resolve(usersDB, {
-            ok: true
-          });
-        })
+          ...dataUSer
+        };
+        User.findOneAndUpdate(
+          {
+            email: dataUSer.email
+          },
+          newArray,
+          null,
+          (err, doc, any) => {
+            return resolve(usersDB, {
+              ok: true
+            });
+          }
+        );
       }
     });
-});
+  });
 
-exports.getUSerByCedulaPromise = (cedula) => new Promise((resolve, reject) => {
-  User.find({
+exports.getUSerByCedulaPromise = cedula =>
+  new Promise((resolve, reject) => {
+    User.find({
       cedula
-    })
-    .exec((err, usersDB) => {
+    }).exec((err, usersDB) => {
       if (err) {
         let err = {
           ok: false,
-          err,
-        }
+          err
+        };
         return reject(err);
       }
       if (usersDB.length === 0) {
-
         const error = {
           ok: false,
           message: 'no existe el usuario mi rey',
-          status: 400,
+          status: 400
         };
         return reject(error);
       }
@@ -154,35 +175,41 @@ exports.getUSerByCedulaPromise = (cedula) => new Promise((resolve, reject) => {
         ok: true
       });
     });
-});
-
-exports.deleteUSerPromise = id => new Promise((resolve, reject) => {
-  User.findByIdAndUpdate(id, {
-    state: false
-  }, {
-    new: true
-  }, (err, userDB) => {
-    if (err) {
-      let err = {
-        ok: false,
-        err,
-      }
-      return reject(err);
-    }
-    if (!userDB) {
-      let err = {
-        status: 400,
-        ok: false,
-        err: {
-          message: 'El usuario no existe padre',
-        },
-      }
-      return reject(err)
-    }
-    let ok = {
-      ok: true,
-      delete: userDB,
-    }
-    return resolve(ok)
   });
-});
+
+exports.deleteUSerPromise = id =>
+  new Promise((resolve, reject) => {
+    User.findByIdAndUpdate(
+      id,
+      {
+        state: false
+      },
+      {
+        new: true
+      },
+      (err, userDB) => {
+        if (err) {
+          let err = {
+            ok: false,
+            err
+          };
+          return reject(err);
+        }
+        if (!userDB) {
+          let err = {
+            status: 400,
+            ok: false,
+            err: {
+              message: 'El usuario no existe padre'
+            }
+          };
+          return reject(err);
+        }
+        let ok = {
+          ok: true,
+          delete: userDB
+        };
+        return resolve(ok);
+      }
+    );
+  });
